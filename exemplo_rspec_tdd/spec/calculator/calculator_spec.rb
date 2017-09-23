77ac315e130e3d10fc5c8a7144bb9e30209a9c48
Diego Novais <contato@diegonovais.com.br>
2017-09-23 19:16:12 -0300

    Understand and learning about it, xit e others tricks

--- exemplo_rspec_tdd/spec/calculator/calculator_spec.rb
+++ exemplo_rspec_tdd/spec/calculator/calculator_spec.rb
@@ -7,27 +7,44 @@ describe Calculator do
 	# Fazendo o setup usando subject como outro nome
 	subject(:calc) { described_class.new() }
 
-	# Descricao do que vc espera
-	it 'use sum method for 2 numbers' do
-		
-		##### Setup #####
-		# usando o subject, não é necessário instanciar a classe, pois quando se usa o subject, ja subentende
-		# que esta sendo referenciado uma instancia da classe.
-		#calc = Calculator.new
-		
-		##### Exercise ##### 
-		#result = subject.sum(5, 7)
-		result = calc.sum(5, 7)
+	# O teste da classe Calculator no contexto do metodo sum
+	context '#sum' do
+		# Descricao do que vc espera
+		it 'for 2 numbers' do
+			
+			##### Setup #####
+			# usando o subject, não é necessário instanciar a classe, pois quando se usa o subject, ja subentende
+			# que esta sendo referenciado uma instancia da classe.
+			#calc = Calculator.new
+			
+			##### Exercise ##### 
+			#result = subject.sum(5, 7)
+			result = calc.sum(5, 7)
 
-		###### Verify #####
- 		# O resultado deve ser igual a 11
-		expect(result).to eq(12)
-	end
+			###### Verify #####
+			# O resultado deve ser igual a 11
+			expect(result).to eq(12)
+		end
 
-	it 'use sum method for 2 numbers negative numbers' do
-		#calc = Calculator.new
-		#result = subject.sum(-5, 7)
-		result = calc.sum(-5, 7)
-		expect(result).to eq(2)
+		it 'for 2 numbers, one negative, and other, positive numbers' do
+			result = calc.sum(-5, 7)
+			expect(result).to eq(2)
+		end
+
+		# Se deixar o teste assim, ele ira aparecer como pendente.
+		#it 'for 2 numbers negatives'
+		
+		# Outra forma de deixar o teste como pendente ou cancelado, seria usar o x antes do it
+		xit 'for 2 numbers negatives' do
+			result = calc.sum(-5, -7)
+			expect(result).to eq(2)
+		end
+
+		#### Dicas ###
+		# Para rodar uma linha especifica do test 
+			#spec spec/calculator/calculator_spec.rb:15
+		# Para rodar um teste especifico pelo seu titulo
+			#spec spec/calculator/calculator_spec.rb -e "for 2 numbers, one negative, and other, positive numbers"
+
 	end
 end
