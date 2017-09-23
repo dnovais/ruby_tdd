2350fdf7736c0d19140ebe773ca391429816eec1
Diego Novais <contato@diegonovais.com.br>
2017-09-23 18:55:57 -0300

    Learning about subject

--- exemplo_rspec_tdd/spec/calculator/calculator_spec.rb
+++ exemplo_rspec_tdd/spec/calculator/calculator_spec.rb
@@ -3,13 +3,20 @@ require 'calculator'
 
 #Descreve o teste
 describe Calculator do
+
+	# Fazendo o setup usando subject como outro nome
+	subject(:calc) { described_class.new() }
+
 	# Descricao do que vc espera
 	it 'use sum method for 2 numbers' do
 		
 		##### Setup #####
-		calc = Calculator.new
+		# usando o subject, não é necessário instanciar a classe, pois quando se usa o subject, ja subentende
+		# que esta sendo referenciado uma instancia da classe.
+		#calc = Calculator.new
 		
 		##### Exercise ##### 
+		#result = subject.sum(5, 7)
 		result = calc.sum(5, 7)
 
 		###### Verify #####
@@ -18,7 +25,8 @@ describe Calculator do
 	end
 
 	it 'use sum method for 2 numbers negative numbers' do
-		calc = Calculator.new
+		#calc = Calculator.new
+		#result = subject.sum(-5, 7)
 		result = calc.sum(-5, 7)
 		expect(result).to eq(2)
 	end
