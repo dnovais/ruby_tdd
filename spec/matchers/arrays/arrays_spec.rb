a178d12ac7374d0c87043cf52961891015088ce9
Diego Novais <contato@diegonovais.com.br>
2017-10-17 19:19:03 -0200

    Learning about matchers to array

---
+++ spec/matchers/arrays/arrays_spec.rb
@@ -0,0 +1,20 @@
+describe Array.new([1, 2, 3]), "Matchers to Array" do
+  context 'Testing the matcher include' do
+    it '#include' do
+      expect(subject).to include(2)
+    end
+  end
+
+  context 'Testing the matcher contain_exactly' do
+    it '#contain_exactly' do
+      expect(subject).to contain_exactly(1, 2, 3)
+    end
+  end
+  
+  context 'Testing the matcher match_array' do
+    it '#match_array' do
+      # The matcher match_array expect the complete array (a alias to matcher - contain_exactly)
+      expect(subject).to match_array([1, 2, 3])
+    end
+  end
+end
