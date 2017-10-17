0432eb05b90a9ba1a471ee77f79055a11cd822bd
Diego Novais <contato@diegonovais.com.br>
2017-10-17 19:57:44 -0200

    Learning about the matcher cover to ranges

---
+++ spec/matchers/ranges/ranges_spec.rb
@@ -0,0 +1,8 @@
+describe (1..5),'Matchers to Range' do
+  context 'Testing the matcher Cover to Range' do
+    it '#cover' do
+      expect(subject).to cover(2, 5)
+      expect(subject).not_to cover(0, 6)
+    end
+  end
+end
